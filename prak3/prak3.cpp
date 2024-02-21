7db6c1922666ab091d1a371a46978dff29a2efec
Ethereal <ersd.etvdr.00@mail.ru>
2024-02-21 22:04:38 +0300

    Добавьте файлы проекта.

---
+++ prak3/prak3.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+static void inputMatrix(double matrix[3][5], int three, int five) {
+    for (int i = 0; i < three; ++i) {
+        cout << "Введите элементы " << i + 1 << "-й строки: ";
+        for (int j = 0; j < five; ++j) {
+            cin >> matrix[i][j];
+        }
+    }
+}
+
+double averageOfRow(const double row[], int five) {
+    double sum = 0.0;
+    for (int i = 0; i < five; ++i) {
+        sum += row[i];
+    }
+    return sum / five;
+}
+
+int main() {
+    SetConsoleCP(1251);
+    setlocale(0, "Russian");
+
+    const int three = 3;
+    const int five = 5;
+
+    double matrix[3][5];
+    inputMatrix(matrix, three, five);
+
+    for (int i = 0; i < three; ++i) {
+        double average = averageOfRow(matrix[i], five);
+        cout << "Среднее арифметическое элементов " << i + 1 << "-й строки: " << average << endl;
+    }
+
+    return 0;
+}
